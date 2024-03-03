cb60e16ff910893e56cce10f4e36451b131d4b4c
Eloy Pérez <ej.perezgomez@gmail.com>
2024-03-03 10:32:41 +0100

    Diamond collected sound

--- scripts/diamond.gd
+++ scripts/diamond.gd
@@ -3,12 +3,12 @@ class_name Diamond extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var area: Area2D = $AnimatedSprite2D/Area2D
 @onready var shape: CollisionShape2D = $AnimatedSprite2D/Area2D/CollisionShape2D
-
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready() -> void:
 	area.area_entered.connect(_on_area_entered)
 
-
 func _on_area_entered(_area2d: Area2D) -> void:
 	shape.call_deferred("set_disabled", true)
 	animation_player.play("pickup")
+	audio_player.play()
